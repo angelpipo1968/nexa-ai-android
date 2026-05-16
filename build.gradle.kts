3cd9037c465150372e4f9628b7550cc453dd63d1
angelpipo1968 <angelpipo1968@gmail.com>
2026-05-16 11:48:36 -0700

    Update to Kotlin 2.1.0 and AGP 8.8.0 to resolve extension conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.7" apply false
+    id("com.android.application") version "8.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }
